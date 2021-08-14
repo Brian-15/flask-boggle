6de133c3ab6b0295313f1ae1a954c75cb86c139a
Brian-15 <bcirieco@gmail.com>
2021-08-13 17:04:41 -0700

    created static and templates folder, and added some basic Jinja routing and templating

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 from boggle import Boggle
+from flask import Flask, request, render_template
+app = Flask(__name__)
 
 boggle_game = Boggle()
+
+@app.route('/')
+def home():
+    render_template("board.html")
