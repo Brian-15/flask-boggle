a194052c146ab55d28d9aac78abf393c3e8f836a
Brian-15 <bcirieco@gmail.com>
2021-08-13 17:34:09 -0700

    generated board that is stored in session storage

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from boggle import Boggle
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "happy_panda"
 
 boggle_game = Boggle()
 
+@app.before_first_request
+def setup():
+    session["board"] = boggle_game.make_board()
+    return redirect('/')
+
 @app.route('/')
 def home():
-    render_template("board.html")
+    return render_template("board.html", board=session["board"])
