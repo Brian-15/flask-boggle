a1d14aa692fee0c5162d9ab8112ac3b96e08144c
Brian-15 <bcirieco@gmail.com>
2021-08-14 19:05:28 -0700

    added scoring system

--- static/app.js
+++ static/app.js
@@ -1,12 +1,13 @@
 
-const BASE_URL = 'http://127.0.0.1:5000'
-const $msg = $('#messages')
-const $input = $('input')
+const BASE_URL = 'http://127.0.0.1:5000';
+const $msg = $('#messages');
+const $guess = $('#guess');
+const $score = $('#score');
 
 async function handleGuess(evt) {
     evt.preventDefault();
-    const word = $input.val();
-    $input.val('');
+    const word = $guess.val();
+    $guess.val('');
 
     const response = await axios({
         url: `${BASE_URL}/guess`,
@@ -18,9 +19,16 @@ async function handleGuess(evt) {
 
     const answer = response.data.result;
     
+    // update message for user
     $msg.text(`${word} is ${answer.replace(/-/g, ' ')}`);
 
+    // add score if valid word
+    if (answer == 'ok') $score.text(Number(score.text()) + word.length);
+
     return answer;
+}
+
+function addScore(word) {
 }
 
 $('form').on('submit', handleGuess)
