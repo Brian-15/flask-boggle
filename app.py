1175ffb00c5d90129fed7a600072630bb9c9c620
Brian-15 <bcirieco@gmail.com>
2021-08-16 13:54:47 -0700

    back-end handles duplicate guesses, and thus will not count them towards the score

--- app.py
+++ app.py
@@ -4,15 +4,21 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "happy_panda"
 
 boggle_game = Boggle()
+guesses = []
 
 @app.before_first_request
 def setup():
+    """
+        Initializes board from boggle class method, and stores high score and play count variables inside 
+        of session object as 0 if not present
+    """
     session["board"] = boggle_game.make_board()
     session["highscore"] = session.get("highscore", 0)
     session["play_count"] = session.get("play_count", 0)
 
 @app.route('/')
 def home():
+    """Renders main page by using variables stored in session storages"""
     return render_template("index.html",
                             board=session["board"],
                             highscore=session["highscore"],
@@ -21,14 +27,20 @@ def home():
 
 @app.route('/guess', methods = ['POST'])
 def guess():
-
+    """route that handles AJAX post request for guesses as well as duplicates.
+    Calculates whether word is valid, and returns message result"""
     word = request.form["guess"]
-    result = boggle_game.check_valid_word(session["board"], word)
 
-    return jsonify(result=result)
+    if word in guesses:
+        return jsonify(result="already-guessed")
+    else:
+        guesses.append(word)
+        return jsonify(result=boggle_game.check_valid_word(session["board"], word))
 
 @app.route('/game-over', methods=['POST'])
 def game_over():
+    """route that stores high score and increments game play count by one"""
+
     data = request.get_json()
     session["highscore"] = int(data["highscore"])
 
@@ -36,6 +48,6 @@ def game_over():
 
     session["board"] = boggle_game.make_board()
 
-    home()
+    guesses = []
 
     return jsonify(count=session["play_count"])
